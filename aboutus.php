e8c0f3080243b425ad9f8e7e18e48f7123390920
Khánh <77840594+sshinichi-69@users.noreply.github.com>
2022-12-14 09:00:19 +0700

    Update aboutus.php

--- aboutus.php
+++ aboutus.php
@@ -21,21 +21,17 @@
 <body>
     <div>
         <?php headerHTML(0) ?>
-        <div>
-            <div id="header" class="nav"></div>
-            <div id="navigation" class="nav"></div>
-            <div id="aboutus-slider" class="white-text">
-                <div class="content">
-                    <div class="uppercase pr">GIỚI THIỆU VỀ LOGITECH</div>
-                </div>
+        <div id="aboutus-slider" class="white-text">
+            <div class="content">
+                <div class="uppercase pr">GIỚI THIỆU VỀ LOGITECH</div>
             </div>
-            <div id="aboutus-introduce">
-                <div class="content">
-                    <div class="uppercase title">CÂU CHUYỆN CỦA CHÚNG TÔI</div>
-                    <div class="description">Là một công ty của Thụy Sĩ tập trung vào đổi mới và chất lượng, Logitech thiết kế ra các sản phẩm và trải nghiệm có mặt trong cuộc sống hàng ngày của mọi người. Được thành lập vào năm 1981 tại Lausanne, Thụy Sĩ và nhanh chóng mở rộng tới Thung lũng Silicon, Logitech bắt đầu kết nối mọi người thông qua các thiết bị ngoại vi máy tính sáng tạo và nhiều sản phẩm hàng đầu trong ngành, bao gồm chuột không dây hồng ngoại, trackball hoạt động bằng ngón tay cái, chuột laser, v.v.</div>
-                    <div class="description">Kể từ những ngày đầu tiên, chúng tôi đã mở rộng cả chuyên môn về thiết kế sản phẩm và phạm vi toàn cầu của chúng tôi. Đối với mỗi sản phẩm chúng tôi đều tập trung vào cách khách hàng kết nối và tương tác với thế giới kỹ thuật số. Chúng tôi lấy thiết kế làm trung tâm của tất cả mọi thứ mà chúng tôi tạo ra, trong mỗi nhóm và mọi quy tắc, để tạo ra những trải nghiệm thực sự độc đáo và có ý nghĩa.</div>
-                    <div class="description">Với các sản phẩm được bán ở hầu hết các quốc gia trên thế giới, Logitech đã phát triển thành một công ty đa thương hiệu thiết kế các sản phẩm kết nối con người lại với nhau thông qua âm nhạc, chơi game, video và máy tính. Các thương hiệu của Logitech bao gồm Logitech, Logitech G, ASTRO Gaming, Ultimate Ears, Jaybird, Blue Microphones và Streamlabs.</div>
-                </div>
+        </div>
+        <div id="aboutus-introduce">
+            <div class="content">
+                <div class="uppercase title">CÂU CHUYỆN CỦA CHÚNG TÔI</div>
+                <div class="description">Là một công ty của Thụy Sĩ tập trung vào đổi mới và chất lượng, Logitech thiết kế ra các sản phẩm và trải nghiệm có mặt trong cuộc sống hàng ngày của mọi người. Được thành lập vào năm 1981 tại Lausanne, Thụy Sĩ và nhanh chóng mở rộng tới Thung lũng Silicon, Logitech bắt đầu kết nối mọi người thông qua các thiết bị ngoại vi máy tính sáng tạo và nhiều sản phẩm hàng đầu trong ngành, bao gồm chuột không dây hồng ngoại, trackball hoạt động bằng ngón tay cái, chuột laser, v.v.</div>
+                <div class="description">Kể từ những ngày đầu tiên, chúng tôi đã mở rộng cả chuyên môn về thiết kế sản phẩm và phạm vi toàn cầu của chúng tôi. Đối với mỗi sản phẩm chúng tôi đều tập trung vào cách khách hàng kết nối và tương tác với thế giới kỹ thuật số. Chúng tôi lấy thiết kế làm trung tâm của tất cả mọi thứ mà chúng tôi tạo ra, trong mỗi nhóm và mọi quy tắc, để tạo ra những trải nghiệm thực sự độc đáo và có ý nghĩa.</div>
+                <div class="description">Với các sản phẩm được bán ở hầu hết các quốc gia trên thế giới, Logitech đã phát triển thành một công ty đa thương hiệu thiết kế các sản phẩm kết nối con người lại với nhau thông qua âm nhạc, chơi game, video và máy tính. Các thương hiệu của Logitech bao gồm Logitech, Logitech G, ASTRO Gaming, Ultimate Ears, Jaybird, Blue Microphones và Streamlabs.</div>
             </div>
         </div>
     </div>
